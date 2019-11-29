c2a4a22aadc8f0d62dd11f26c8f783b79a57689b
wangshuyu <sureforwsy@163.com>
2019-11-29 18:03:37 +0800

    转移一下

--- src/app/core/components/footer.ts
+++ src/app/core/components/footer.ts
@@ -5,7 +5,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   template: `
     <mat-toolbar color="primary">
       <span class="fill-remaining-space"></span>
-      <span>&copy; 2017 版权所有: 接灰的电子产品</span>
+      <span>&copy; BrightWhisper</span>
       <span class="fill-remaining-space"></span>
     </mat-toolbar>
   `,
